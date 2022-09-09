6f327a27b97659addc30ad42b06e48102ed00cb7
KeyurMistry <keyurmist@gmail.com>
2022-09-09 23:45:26 +0100

    intergrate middleware

--- server/server.js
+++ server/server.js
@@ -3,13 +3,21 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+server.applyMiddleware({ app });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
