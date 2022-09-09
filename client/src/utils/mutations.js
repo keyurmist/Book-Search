44f2b3403bba67e73e13d6ae0895084b48b59a3c
KeyurMistry <keyurmist@gmail.com>
2022-09-09 22:25:51 +0100

    finished mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,50 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]
+    $description: String
+    $title: String
+    $bookId: String
+    $image: String
+    $link: String
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+    ) {
+      _id
+      username
+      savedBooks {
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      username
+      savedBooks {
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+    }
+  }
+`;
