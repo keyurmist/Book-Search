ad7dde84207277a4255527bd3accc4f8c97554e8
KeyurMistry <keyurmist@gmail.com>
2022-09-09 23:42:38 +0100

    import tydef and resolvers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
 
